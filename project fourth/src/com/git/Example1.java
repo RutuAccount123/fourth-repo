2b14582853bcbb0929acb57cad95b7f888d24e81
RutuAccount123 <shubhangikorke0452@gmail.com>
2022-11-15 18:06:15 -0800

    my fourth commit

---
+++ project fourth/src/com/git/Example1.java
@@ -0,0 +1,15 @@
+package com.git;
+
+import java.util.HashMap;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		HashMap hs=new HashMap();
+		
+      hs.put(5,"ram");
+      hs.put(4,"vilas");
+      System.out.println(hs);
+	}
+
+}
